559e050e2e4a74453d35a8572e36ba73074fd316
JoaquinMaidana <joacomaidana11@gmail.com>
2022-06-22 15:02:05 -0300

    metodos categoria

--- Classes/Categoria.h
+++ Classes/Categoria.h
@@ -5,13 +5,20 @@
 #ifndef LAB_3_PROYECTO_CATEGORIA_H
 #define LAB_3_PROYECTO_CATEGORIA_H
 #include <string>
-
+#include <iostream>
 using namespace std;
 
 class Categoria {
 private:
     string nombre;
-    string desc;
+    string descripcion;
+public:
+    Categoria(string nombre,string descripcion);
+    virtual ~Categoria();
+    string getNombre();
+    string getDesc();
+    void setNombre(string nombre);
+    void setDescripcion(string descripcion);
 };
 
 #endif //LAB_3_PROYECTO_CATEGORIA_H
