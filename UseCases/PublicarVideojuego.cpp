1d59178e734e625bf7aef6d3d1885f65df2a9ec1
kitorosano <rosanoesteban2@gmail.com>
2022-07-13 23:33:05 -0300

    preparando caso de uso

--- UseCases/PublicarVideojuego.cpp
+++ UseCases/PublicarVideojuego.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "PublicarVideojuego.h"
+#include "Factory/Factory.h"
+#include <iostream>
+#include <string>
+
+void PublicarVideojuego() {
+    Factory *fabrica;
+
+    string nombre, descripcion;
+    double costoMensual, costoTrimestral, costoAnual, costoVitalicia;
+
+    cout<<"|------------------------------------------------|"<<endl;
+    cout<<"|               PUBLICAR VIDEOJUEGO              |"<<endl;
+    cout<<"|------------------------------------------------|"<<endl<<endl;
+
+};
