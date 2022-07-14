0f3b3ba84ec58395d232d5031c8d475ca1d80fe6
kitorosano <rosanoesteban2@gmail.com>
2022-07-13 23:06:59 -0300

    Terminado caso de uso

--- UseCases/AgregarCategoria.cpp
+++ UseCases/AgregarCategoria.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "AgregarCategoria.h"
-#include "/Factory/Factory.h"
+#include "Factory/Factory.h"
 #include <iostream>
 #include <string>
 using namespace std;
@@ -24,7 +24,7 @@ void AgregarCategoria() {
         }
 
         cout<<"|------------------------------------------------|"<<endl<<endl;
-    } catch (invalid_argument &e) {
+    } catch (invalid_argument &e) { // ERROR: El usuario no es desarrollador
         cout << e.what() << endl;
         return;
     }
@@ -43,55 +43,66 @@ void AgregarCategoria() {
     string descripcionCategoria;
     cin>>descripcionCategoria;
 
-    int opcion;
-    do {
-        cout << "|------------------------------------------------|" << endl;
-        cout << "|                AGREGAR CATEGORÍA               |" << endl;
-        cout << "|------------------------------------------------|" << endl;
-        cout << "| 1. Agregar categoría de plataforma             |" << endl;
-        cout << "| 2. Agregar categoría de género                 |" << endl;
-        cout << "| 2. Agregar categoría de otro tipo              |" << endl;
-        cout << "|------------------------------------------------|" << endl << endl;
-        cin >> opcion;
+    try {
+        int opcion;
+        do {
+            cout << "|------------------------------------------------|" << endl;
+            cout << "|                AGREGAR CATEGORÍA               |" << endl;
+            cout << "|------------------------------------------------|" << endl;
+            cout << "| 1. Agregar categoría de plataforma             |" << endl;
+            cout << "| 2. Agregar categoría de género                 |" << endl;
+            cout << "| 2. Agregar categoría de otro tipo              |" << endl;
+            cout << "|------------------------------------------------|" << endl << endl;
+            cin >> opcion;
 
-        switch (opcion) {
-            case 1:
-                factory->getInstance()->getInterfaceC()->agregarCategoria(nombreCategoria, descripcionCategoria, E_TipoCategoria::PLATAFORMA);
-                break;
-            case 2:
-                factory->getInstance()->getInterfaceC()->agregarCategoria(nombreCategoria, descripcionCategoria, E_TipoCategoria::GENERO);
-                break;
-            case 3:
-                factory->getInstance()->getInterfaceC()->agregarCategoria(nombreCategoria, descripcionCategoria, E_TipoCategoria::OTRO);
-                break;
-            default:
-                cout << "Opción inválida!" << endl;
-                break;
-        }
-    }while(opcion !=1 && opcion !=2 && opcion !=3);
+            switch (opcion) {
+                case 1:
+                    factory->getInstance()->getInterfaceC()->agregarCategoria(nombreCategoria, descripcionCategoria, E_TipoCategoria::PLATAFORMA);
+                    break;
+                case 2:
+                    factory->getInstance()->getInterfaceC()->agregarCategoria(nombreCategoria, descripcionCategoria, E_TipoCategoria::GENERO);
+                    break;
+                case 3:
+                    factory->getInstance()->getInterfaceC()->agregarCategoria(nombreCategoria, descripcionCategoria, E_TipoCategoria::OTRO);
+                    break;
+                default:
+                    cout << "Opción inválida!" << endl;
+                    break;
+            }
+        } while(opcion !=1 && opcion !=2 && opcion !=3);
+    } catch (invalid_argument &e) { // ERROR: El usuario no es desarrollador
+        cout << e.what() << endl;
+        return;
+    }
 
     cout<<endl<<"|------------------------------------------------|"<<endl<<endl;
 
-    do {
-        cout << "|------------------------------------------------|" << endl;
-        cout << "|                AGREGAR CATEGORÍA               |" << endl;
-        cout << "|------------------------------------------------|" << endl;
-        cout << "| 1. Confirmar categoría                         |" << endl;
-        cout << "| 2. Cancelar categoría                          |" << endl;
-        cout << "|------------------------------------------------|" << endl << endl;
-        cin >> opcion;
+    try {
+        int opcion;
+        do {
+            cout << "|------------------------------------------------|" << endl;
+            cout << "|                AGREGAR CATEGORÍA               |" << endl;
+            cout << "|------------------------------------------------|" << endl;
+            cout << "| 1. Confirmar categoría                         |" << endl;
+            cout << "| 2. Cancelar categoría                          |" << endl;
+            cout << "|------------------------------------------------|" << endl << endl;
+            cin >> opcion;
 
-        switch (opcion) {
-            case 1:
-                factory->getInstance()->getInterfaceC()->confirmarCategoria();
-                break;
-            case 2:
-                factory->getInstance()->getInterfaceC()->cancelarCategoria();
-                break;
-            default:
-                cout << "Opción inválida!" << endl;
-                break;
-        }
-    }while(opcion!=1 && opcion !=2);
+            switch (opcion) {
+                case 1:
+                    factory->getInstance()->getInterfaceC()->confirmarCategoria();
+                    break;
+                case 2:
+                    factory->getInstance()->getInterfaceC()->cancelarCategoria();
+                    break;
+                default:
+                    cout << "Opción inválida!" << endl;
+                    break;
+            }
+        }while(opcion!=1 && opcion !=2);
+    } catch (invalid_argument &e) { // ERROR: El usuario no es desarrollador
+        cout << e.what() << endl;
+        return;
+    }
 
 }
