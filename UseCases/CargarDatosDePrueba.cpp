0debaed5aa526652475708e4ecb396b2b0c9f4a6
rafa5551 <rafa5551@hotmail.com>
2022-07-14 17:25:58 -0300

    Caso de uso cargar datos x2

--- UseCases/CargarDatosDePrueba.cpp
+++ UseCases/CargarDatosDePrueba.cpp
@@ -28,5 +28,34 @@ void CargarDatosDePrueba() {
     factory->getInterfaceU()->registroJugador("camila@mail.com","123","camila","algo");
     factory->getInterfaceU()->confirmarRegistro();
 
+    // Categorias
+    factory->getInterfaceC()->agregarCategoria("PC","descripción libre",PLATAFORMA);
+    factory->getInterfaceC()->confirmarCategoria();
+    factory->getInterfaceC()->agregarCategoria("PS4","descripción libre",PLATAFORMA);
+    factory->getInterfaceC()->confirmarCategoria();
+    factory->getInterfaceC()->agregarCategoria("Xbox One","descripción libre",PLATAFORMA);
+    factory->getInterfaceC()->confirmarCategoria();
+    factory->getInterfaceC()->agregarCategoria("Deporte","descripción libre",GENERO);
+    factory->getInterfaceC()->confirmarCategoria();
+    factory->getInterfaceC()->agregarCategoria("Supervivencia","descripción libre",GENERO);
+    factory->getInterfaceC()->confirmarCategoria();
+    factory->getInterfaceC()->agregarCategoria("Estrategia","descripción libre",GENERO);
+    factory->getInterfaceC()->confirmarCategoria();
+    factory->getInterfaceC()->agregarCategoria("Teen","Su contenido está dirigido a jóvenes de 13 años en adelante",OTRO);
+    factory->getInterfaceC()->confirmarCategoria();
+    factory->getInterfaceC()->agregarCategoria("E","Su contenido está dirigido para todo público",OTRO);
+    factory->getInterfaceC()->confirmarCategoria();
+    factory->getInterfaceC()->agregarCategoria("Acción","descripción libre",GENERO);
+    factory->getInterfaceC()->confirmarCategoria();
+    factory->getInterfaceC()->agregarCategoria("Aventura","descripción libre",GENERO);
+    factory->getInterfaceC()->confirmarCategoria();
+    factory->getInterfaceC()->agregarCategoria("Switch","descripción libre",PLATAFORMA);
+    factory->getInterfaceC()->confirmarCategoria();
+    factory->getInterfaceC()->agregarCategoria("Xbox X","descripción libre",PLATAFORMA);
+    factory->getInterfaceC()->confirmarCategoria();
+    factory->getInterfaceC()->agregarCategoria("PS5","descripción libre",PLATAFORMA);
+    factory->getInterfaceC()->confirmarCategoria();
+
+
 
 }
